48983941afafa9a7192e77812d770ae785149f9e
leibou <niangibou92@gmail.com>
2018-12-12 21:28:12 +0000

    Revert "Final Commit"
    This reverts commit b0bb4fec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ArticlesComponent } from './articles/articles.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import {HttpClientModule} from '@angular/common/http';
-import {ArticlesServices} from './services/articles.services';
-import {FormsModule} from '@angular/forms';
-import { AddarticleComponent } from './addarticle/addarticle.component';
-import { EditArticleComponent } from './edit-article/edit-article.component';
-
-
-
-const shopRoutes: Routes = [
-  {path: 'articles', component: ArticlesComponent},
-  {path: 'editarticle/:id', component: EditArticleComponent},
-  {path: '', redirectTo: '/articles', pathMatch: 'full'},
-  {path: 'new-article', component: AddarticleComponent}
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ArticlesComponent,
-    NavigationComponent,
-    AddarticleComponent,
-    EditArticleComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule, RouterModule.forRoot(shopRoutes), HttpClientModule, FormsModule
+    BrowserModule
   ],
-  providers: [ArticlesServices],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/models/articles.model.ts
+++
@@ -1,8 +0,0 @@
-export class ArticlesModel {
-  id = null;
-  titre = '';
-  description = '';
-  prix = 0;
-  photo = '';
-  id_cat = null;
-}
