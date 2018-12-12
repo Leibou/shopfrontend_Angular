b0bb4fec662871a757c5f0bf217fdaedaa387f5c
leibou <niangibou92@gmail.com>
2018-12-12 21:17:40 +0000

    Final Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ArticlesServices} from './services/articles.services';
+import {FormsModule} from '@angular/forms';
+import { AddarticleComponent } from './addarticle/addarticle.component';
+import { EditArticleComponent } from './edit-article/edit-article.component';
+
+
+
+const shopRoutes: Routes = [
+  {path: 'articles', component: ArticlesComponent},
+  {path: 'editarticle/:id', component: EditArticleComponent},
+  {path: '', redirectTo: '/articles', pathMatch: 'full'},
+  {path: 'new-article', component: AddarticleComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticlesComponent,
+    NavigationComponent,
+    AddarticleComponent,
+    EditArticleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(shopRoutes), HttpClientModule, FormsModule
   ],
-  providers: [],
+  providers: [ArticlesServices],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/models/articles.model.ts
@@ -0,0 +1,8 @@
+export class ArticlesModel {
+  id = null;
+  titre = '';
+  description = '';
+  prix = 0;
+  photo = '';
+  id_cat = null;
+}
